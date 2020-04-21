411c1bb56df8bde91f1cd1cf188f5a5285e56059
Jayson Ragasa <jayson.d.ragasa@live.com>
2020-04-21 14:49:17 +0800

    added TextLine and SpanLine/RunLine so you can create multiline tests using TextBlock/Label and in Run/Span

---
+++ JaraIOExtensionsNetStandard/TextBlockExtension.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Jara.Xaml.Control.Extension
+{
+    public class TextEx
+    {
+        #region TextLine
+        public static readonly DependencyProperty TextLineProperty = DependencyProperty.RegisterAttached(
+            "TextLine",
+            typeof(string),
+            typeof(TextBlock),
+            new PropertyMetadata("\n", OnTextLinePropertyChanged));
+
+        public static string GetTextLine(DependencyObject element)
+        {
+            return (string)element.GetValue(TextLineProperty);
+        }
+
+        public static void SetTextLine(DependencyObject element, string value)
+        {
+            element.SetValue(TextLineProperty, value);
+        }
+
+        private static void OnTextLinePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+            var element = d as TextBlock;
+
+            if (element != null)
+            {
+                string newText = NewText(args.NewValue.ToString());
+
+                element.Text = $"{newText}";
+            }
+        }
+        #endregion
+
+        #region RunLine
+        public static readonly DependencyProperty RunLineProperty = DependencyProperty.RegisterAttached(
+            "RunLine",
+            typeof(string),
+            typeof(Run),
+            new PropertyMetadata("\n", OnRunLinePropertyChanged));
+
+        public static string GetRunLine(DependencyObject element)
+        {
+            return (string)element.GetValue(RunLineProperty);
+        }
+
+        public static void SetRunLine(DependencyObject element, string value)
+        {
+            element.SetValue(RunLineProperty, value);
+        }
+
+        private static void OnRunLinePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+            var element = d as Run;
+
+            if (element != null)
+            {
+                string newText = NewText(args.NewValue.ToString());
+
+                element.Text = $"\n{newText}";
+            }
+        }
+        #endregion
+
+        static string NewText(string newValue)
+        {
+            string[] texts = newValue.Split(new string[] { "\\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string newText = texts.Length > 1 ? string.Join("\r\n", texts) : newValue;
+
+            return newText.Trim();
+        }
+    }
+}
---
+++ JaraIOExtensionsXamarin/LabelExtension.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Jara.Xaml.Control.Extension
+{
+    public class TextEx
+    {
+        #region TextBlock
+        public static readonly BindableProperty TextLineProperty = BindableProperty.CreateAttached("TextLine", typeof(string), typeof(Span), "\r\n", defaultBindingMode: BindingMode.TwoWay, validateValue: null, propertyChanged: OnTextLinePropertyChanged);
+
+        public static string GetTextLine(BindableObject element)
+        {
+            return (string)element.GetValue(TextLineProperty);
+        }
+
+        public static void SetTextLine(BindableObject element, string value)
+        {
+            element.SetValue(TextLineProperty, value);
+        }
+
+        private static void OnTextLinePropertyChanged(BindableObject obj, object oldValue, object newValue)
+        {
+            var element = obj as Span;
+
+            if (element != null && newValue != null)
+            {
+                string newText = NewText(newValue.ToString());
+
+                element.Text = $"{newText}";
+            }
+        }
+        #endregion
+
+        #region Span
+        public static readonly BindableProperty SpanLineProperty = BindableProperty.CreateAttached("SpanLine", typeof(string), typeof(Span), "\r\n", defaultBindingMode: BindingMode.TwoWay, validateValue: null, propertyChanged: OnSpanLinePropertyChanged);
+
+        public static string GetSpanLine(BindableObject element)
+        {
+            return (string)element.GetValue(SpanLineProperty);
+        }
+
+        public static void SetSpanLine(BindableObject element, string value)
+        {
+            element.SetValue(SpanLineProperty, value);
+        }
+
+        private static void OnSpanLinePropertyChanged(BindableObject obj, object oldValue, object newValue)
+        {
+            var element = obj as Span;
+
+            if (element != null && newValue != null)
+            {
+                string newText = NewText(newValue.ToString());
+
+                element.Text = $"\n{newText}";
+            }
+        }
+        #endregion
+
+        static string NewText(string newValue)
+        {
+            string[] texts = newValue.Split(new string[] { "\\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string newText = texts.Length > 1 ? string.Join("\r\n", texts) : newValue;
+
+            return newText.Trim();
+        }
+    }
+
+    public class LabelEx
+    {
+        
+    }
+
+    //public class SpanEx : IMarkupExtension
+    //{
+    //    public string TextLine { get; set; } = "";
+
+    //    public object ProvideValue(IServiceProvider serviceProvider)
+    //    {
+    //        return $"\n{TextLine}";
+    //    }
+    //}
+}
